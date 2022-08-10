2cbe71765721bd341678dd2c773fa30416b9ec15
Jan-Pieter George <janpieterzoutewelle@gmail.com>
2022-08-10 14:56:01 +0200

    Pipe output

--- app/main.go
+++ app/main.go
@@ -14,6 +14,8 @@ func main() {
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
